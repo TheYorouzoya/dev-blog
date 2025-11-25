a33161942217151467411fb19eeeb39fbc205c6b
Ratnesh Rastogi <ratneshrastogimarch@gmail.com>
2025-11-25 17:47:27 +0000

    Add Frontend Scaffolding

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Topic, Tag
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "topic", "status", "published_at"]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Topic)
+admin.site.register(Tag)
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage
 
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, World! This is the start of my blog app.")
+    ARTICLES_PER_PAGE = 10
+    page_number = request.GET.get('page', 1)
+    articles = Article.objects.filter(status=Article.Status.PUBLISHED)
+    paginator = Paginator(articles, ARTICLES_PER_PAGE)
+
+    try:
+        page_obj = paginator.page(page_number)
+    except EmptyPage as e:
+        return render(request, 'blog/index.html', {"message": str(e)})
+    return render(request, 'blog/index.html', {"page_obj": page_obj})
+
+
+def article(request, article_slug):
+    article = get_object_or_404(Article, slug=article_slug)
+    return render(request, "blog/article.html", {"article": article})
