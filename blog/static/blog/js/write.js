b53fcc221c019ee3bb88af947597709cf391d95d
Ratnesh Rastogi <ratneshrastogimarch@gmail.com>
2025-11-26 09:54:18 +0000

    Replace EasyMDE with Quill on Frontend

--- blog/static/blog/js/write.js
+++ blog/static/blog/js/write.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const editor = new EasyMDE({
-        element: document.getElementById('editor'),
+    const toolbarOptions = [
+        
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link', 'image', 'video'],
+        
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+    ];
+    const quill = new Quill('#editor', {
+        modules: {
+            toolbar: toolbarOptions
+        },
+        theme: 'snow',
     });
 })
